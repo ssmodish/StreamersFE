61e716bb966f960e98219321b64b71b9be64a8dd
Steve Smodish <smodish@hotmail.com>
2020-12-04 17:48:11 -0500

    Changes made for firebase PR

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React App Created!</h1>
+        <h2>Now hosting from Firebase</h2>
       </header>
     </div>
   );
